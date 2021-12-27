b506899347b560e1162f68604cc66158aeb14cf8
KelvinMYYZJ <Kelvin_MYYZJ@outlook.com>
2021-12-27 16:24:35 +0800

    changed default string length to AC

--- src/DefaultConst.cpp
+++ src/DefaultConst.cpp
@@ -2,6 +2,6 @@
 #define BOOKSTORE_SRC_DEFAULTCONST_CPP
 // extern const int string_size = 100;
 // extern const int max_node_size = 10;
-#define string_size 20
-#define max_node_size 4
+#define string_size 100
+#define max_node_size 7
 #endif // BOOKSTORE_SRC_DEFAULTCONST_CPP
